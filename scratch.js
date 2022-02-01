2fbf2fd90121974b3a9d702b6b3255c0afc70d1f
Lance Valle <vallelance8@gmail.com>
2022-02-01 18:11:29 +0800

    initial design

--- scratch.js
+++
@@ -1,8 +0,0 @@
-// //create a simple implementation of grade school classic rock paper scissors.
-
-let computerScore = 0
-let playerScore = 0
-let options = ['rock', 'paper', 'scissors']
-
-// create a function that will randomly select a choice from the array
-function computerChoice
